6dc561f7fca438eca3988446ca850a79e1febcb5
torazuka <torazuka@gmail.com>
2013-07-24 11:51:57 +0900

    changed the Welcome method to return a View object

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -8,19 +8,16 @@ namespace MvcMovie.Controllers
 {
     public class HelloWorldController : Controller
     {
-        //
-        // GET: /HelloWorld/
-
         public ActionResult Index()
         {
             return View();
         }
 
-        // 
-        // GET: /HelloWorld/Welcome
-        public string Welcome(string name, int numTimes = 1)
+        public ActionResult Welcome(string name, int numTimes = 1)
         {
-            return HttpUtility.HtmlEncode("Hello " + name + ", NumTimes is: " + numTimes);
+            ViewBag.Message = "Hello " + name;
+            ViewBag.numTimes = numTimes;
+            return View();
         }
     }
 }

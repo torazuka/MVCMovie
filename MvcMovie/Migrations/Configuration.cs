a1a307433c22a5edc6eb53fba68be41aead95644
torazuka <torazuka@gmail.com>
2013-07-29 13:20:30 +0900

    added DB migration code for a Rating field

--- MvcMovie/Migrations/Configuration.cs
+++ MvcMovie/Migrations/Configuration.cs
@@ -21,23 +21,27 @@ namespace MvcMovie.Migrations
                 Title = "When Harry Met Sally",
                 ReleaseDate = DateTime.Parse("1989-1-11"),
                 Genre = "Romantic Comedy",
+                Rating = "G",
                 Price = 7.99M
             }, new Movie {
                 Title = "Ghostbusters",
                 ReleaseDate = DateTime.Parse("1984-3-13"),
                 Genre = "Comedy",
+                Rating = "G",
                 Price = 8.99M
             }, new Movie
             {
                 Title = "Ghostbusters 2",
                 ReleaseDate = DateTime.Parse("1986-2-23"),
                 Genre = "Comedy",
+                Rating = "G",
                 Price = 9.99M
             }, new Movie
             {
                 Title = "Rio Bravo",
                 ReleaseDate = DateTime.Parse("1959-4-15"),
                 Genre = "Western",
+                Rating = "G",
                 Price = 3.99M
             });
             //  This method will be called after migrating to the latest version.

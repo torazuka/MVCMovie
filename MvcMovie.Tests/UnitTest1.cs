e82185dd84a877ee9d5dd10ba781c00f3bc67fd8
torazuka <torazuka@gmail.com>
2013-07-29 18:45:56 +0900

    added a test project

---
+++ MvcMovie.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcMovie.Models;
+
+namespace MvcMovie.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(0, 0); // for sample
+        }
+    }
+}

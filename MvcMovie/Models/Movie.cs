52b9e4cded846d5ca744383268a925ef7f90c351
torazuka <torazuka@gmail.com>
2013-07-29 15:18:12 +0900

    added annotations for validation

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.ComponentModel.DataAnnotations;
+using System;
 using System.Data.Entity;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +10,21 @@ namespace MvcMovie.Models
     public class Movie
     {
         public int ID { get; set; }
+
+        [Required]
         public string Title { get; set; }
+        
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+        
+        [Required]
         public string Genre { get; set; }
+        
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
+
+        [StringLength(5)]
         public string Rating { get; set; }
     }
 
